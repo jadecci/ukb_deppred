5abd80a656f2298ab103775126019931e5413cb3
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2024-08-12 15:00:32 +0200

    Fix bugs in RSFC extraction code

--- replication/ukb_extract_rsfc.py
+++ replication/ukb_extract_rsfc.py
@@ -9,12 +9,11 @@ def fill_pd_row(
         in_pd: pd.DataFrame, sub: str | int, rsfc_type: str, subject_dir: Path) -> pd.DataFrame:
     rsfc_dir = Path(subject_dir, "ses-2", "non-bids", "fMRI")
     rsfc_file = Path(rsfc_dir, f"sub-{sub}_ses-2_task-rest_{rsfc_type}_correlation_matrix_d100.txt")
-    dl.get(rsfc_file, dataset=subject_dir)
-
-    rsfc_curr = pd.read_table(rsfc_file, delim_whitespace=True, header=None).squeeze()
-    in_pd.loc[sub] = rsfc_curr
-
-    dl.drop(rsfc_file, reckless="kill", dataset=subject_dir)
+    if rsfc_file.is_symlink():
+        dl.get(rsfc_file, dataset=subject_dir)
+        rsfc_curr = pd.read_table(rsfc_file, delimiter="\s+", header=None).squeeze()
+        in_pd.loc[sub] = rsfc_curr
+        dl.drop(rsfc_file, reckless="kill", dataset=subject_dir)
     return in_pd
 
 
@@ -34,14 +33,15 @@ rsfc_part = pd.DataFrame(index=sub_list, columns=range(1485), dtype=float)
 
 # Install dataset
 root_data_dir = Path(args.work_dir, "ukb_raw")
-dl.install(root_data_dir, source=args.url)
+dl.install(root_data_dir, source=args.ukb_raw_url)
 
 for subject in sub_list:
-    sub_dir = Path(args.raw_dir, f"sub-{subject}")
-    dl.get(sub_dir, dataset=root_data_dir, get_data=False)
-
-    rsfc_full = fill_pd_row(rsfc_full, subject, "full", sub_dir)
-    rsfc_part = fill_pd_row(rsfc_part, subject, "partial", sub_dir)
+    sub_dir = Path(root_data_dir, f"sub-{subject}")
+    if sub_dir.exists():
+        dl.get(sub_dir, dataset=root_data_dir, get_data=False)
+        rsfc_full = fill_pd_row(rsfc_full, subject, "full", sub_dir)
+        rsfc_part = fill_pd_row(rsfc_part, subject, "partial", sub_dir)
 
 rsfc_all = rsfc_full.join(rsfc_part, lsuffix="_full", rsuffix="_part")
 rsfc_all.to_csv(args.out_csv)
+dl.remove(dataset=root_data_dir, reckless="kill")
